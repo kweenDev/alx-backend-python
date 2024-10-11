0b9c83472ff680d3ccbde8dd7ad73082af90d7c5
KweenDev <refiloe15radebe@gmail.com>
2024-10-11 03:00:18 +0200

    Implement type-annotated function 'floor' to return the floo of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module defines a function `floor` that returns the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float.
+
+    Parameters:
+    n (float): A float number.
+
+    Returns:
+    int: The floor value of the float.
+    """
+    return math.floor(n)
