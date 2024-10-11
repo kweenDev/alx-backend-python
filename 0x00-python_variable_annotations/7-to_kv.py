e267e28343addab981bf4d0c42f72465579c5fb9
KweenDev <refiloe15radebe@gmail.com>
2024-10-11 03:20:53 +0200

    Implement type-annotated function 'to_kv' to return a tuple with a string and squared int/float

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module defines a function `to_kv` that returns a tuple with a string and the square of a number.
+This module defines a function `to_kv` that
+returns a tuple with a string and the square of a number.
 """
 
 from typing import Union, Tuple
@@ -15,7 +16,8 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     v (Union[int, float]): An integer or float.
 
     Returns:
-    Tuple[str, float]: A tuple where the first element is the string k, and the second element
+    Tuple[str, float]: A tuple where the first element is the
+    string k, and the second element
     is the square of the number v as a float.
     """
     return (k, float(v ** 2))
