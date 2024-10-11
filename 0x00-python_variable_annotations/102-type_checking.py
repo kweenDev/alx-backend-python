78b92310c15ea847e912fe8513afa6525613c604
KweenDev <refiloe15radebe@gmail.com>
2024-10-11 03:44:27 +0200

    Use mypy to validate 'zoom_array' function and apply necessary changes

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,12 +3,13 @@
 This module defines a function `zoom_array` that zooms in a tuple.
 """
 
-from typing import Tuple, List
+from typing import Tuple, List, Any
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    Zooms in a tuple by repeating each item in the tuple a specified number of times.
+    Zooms in a tuple by repeating each item in the
+    tuple a specified number of times.
 
     Parameters:
     lst (Tuple[int, ...]): A tuple of integers.
@@ -17,13 +18,8 @@ def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     Returns:
     List[int]: A list containing the zoomed in values.
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
-
-
-array = (12, 72, 91)
-zoom_2x = zoom_array(array)
-zoom_3x = zoom_array(array, 3)
