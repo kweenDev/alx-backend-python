87f8bcfb08e733bb5aaacf13ac021256a0237363
KweenDev <refiloe15radebe@gmail.com>
2024-11-21 16:05:38 +0200

    feat: add memoization unit tests for utils.memoize decorator. Tested the utils.memoize decorator by verifying that it caches results, minimizing redundant calls to the decorated method.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,8 @@ Unit tests for utils.access_nested_map function.
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
-from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -53,3 +53,27 @@ class TestGetJson(unittest.TestCase):
         result = utils.get_json(test_url)
         self.assertEqual(result, test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test case for utils.memoize decorator.
+    """
+
+    def test_memoize(self):
+        """
+        Test memoization of a method.
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @utils.memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+            self.assertEqual(obj.a_property, 42)
+            self.assertEqual(obj.a_property, 42)
+            mock_method.assert_called_once()
