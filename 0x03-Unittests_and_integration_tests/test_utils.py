e114aa8431637506c02a8d9b508bd79439214560
KweenDev <refiloe15radebe@gmail.com>
2024-11-21 15:53:15 +0200

    feat: add mock-based unit tests for utils.get_json. Added unit tests for unitls.get_json using mocked requests.get to simulate HTTP responses and validate the returned payloads without making actual network calls.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ Unit tests for utils.access_nested_map function.
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -32,3 +33,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test case for utils.get_json function.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Test get_json with mocked HTTP responses.
+        """
+        mock_get.return_value.json.return_value = test_payload
+        result = utils.get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
